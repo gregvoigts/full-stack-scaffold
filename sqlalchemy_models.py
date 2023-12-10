42367a1a2f4457d0815917247db0ed39944de65d
gregvoigts <gregor.voigts@gmail.com>
2023-12-10 23:14:10 +0100

    model creation

--- sqlalchemy_models.py
+++ sqlalchemy_models.py
@@ -1,10 +1,43 @@
-import re
+import re   
 
 def parse_constraint(content):
-    pass
+    match = re.search(
+        r'CONSTRAINT (\w+)_(\w+)_fkey\n\s+FOREIGN KEY\("([^"]+)"\)\s+REFERENCES public\.(\w+)\("([^"]+)"\)\s+ON DELETE (\w+)\s+ON UPDATE (\w+)',
+        content
+    )
+
+    const = {'target':match.group(0),
+             'source':match.group(1),
+             'fk':match.group(2),
+             'ref_tab':match.group(3),
+             'ref_col':match.group(4)}
 
 def parse_column(content):
-    pass
+    col = {'name':content[0],
+           'datatype':content[1],
+           'default':None,
+           'not_null':False,
+           'pk': False,
+           'computed': None
+           }
+    i = 0
+    while i < content.length -1:
+        if content[i].upper() == 'NOT' and content[i+1].upper() == 'NULL':
+            col['not_null'] = True
+            i+=1
+        elif content[i].upper() == 'PRIMARY' and content[i+1].upper() == 'KEY':
+            col['pk'] = True
+            i+=1
+        elif content[i].upper() == 'DEFAULT':
+            col['default'] = content[i+1]
+            i+=1
+        elif content[i].upper() == 'GENERATED':
+            pass
+        elif content[i].upper() == 'REFERENCES':
+            pass
+        else:
+            raise Exception(f'Unkown token in SQL:{content[i]} on column {col["name"]}')
+    
 
 sql_file = "./config/db/initDB.sql"
 out_path = "./backend/src/models.py"
@@ -16,14 +49,21 @@ pattern = re.compile(r'CREATE\s+TABLE\s+IF\s+NOT\s+EXISTS\s+public\."*(\w+)"*\s*
 
 tables = pattern.findall(file)
 
-for table in tables:
-    name = table[0]
+
+for t in tables:
+    table = {'columns':[],'fk':[],'referenzes':[]}
+    table["name"] = t[0]
     contents = re.split(',\n', table[1])
     for content in contents:
-        content_splitted = re.split('\s+', content)
-        if content_splitted[0] == 'UNIQUE':
+        content_splitted = re.split('\s+|(|)', content)
+        if content_splitted[0].upper() == 'UNIQUE':
             pass
-        elif content_splitted[0] == 'CONSTRAINT':
-            parse_constraint(content_splitted)
+        elif content_splitted[0].upper() == 'CONSTRAINT':
+            table['fk'].append(parse_constraint(content))
+        elif content_splitted[0].upper() == 'PRIMARY' and content_splitted[1].upper() == 'KEY':
+            table['pk'].append(parse_constraint(content_splitted))
+            for col in table['columns']:
+                if col['name'] in content_splitted[1]:
+                    col['pk'] = True
         else:
-            parse_column(content_splitted)
+            table['columns'].append(parse_column(content_splitted))
